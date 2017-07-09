347823a621aa78b5b0a6537483916e2e92e8b3d7
jimmio <git-jimmio.o3lma@dralias.com>
2017-07-09 13:40:06 -0400

    aes-encrypt-random successfully makes use of get-random-padding

--- src/crypto_challenges/core.clj
+++ src/crypto_challenges/core.clj
@@ -424,13 +424,42 @@ or a decrypted string"
 
 ;;;; ECB/CBC DETECTION ORACLE ;;;;
 
+
+;; An ECB/CBC detection oracle
+
+;; Now that you have ECB and CBC working:
+
+;; Write a function to generate a random AES key; that's just 16 random bytes.
+
+;; Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
+
+;; The function should look like:
+
+;; encryption_oracle(your-input)
+;; => [MEANINGLESS JIBBER JABBER]
+
+;; Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
+
+;; Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
+
+;; Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
+
+
 (defn gen-aes-key []
-  (let [rand-ints (for [i (range 0 16)] (rand-int 129))]
+  (let [rand-ints (for [i (range 0 16)] (rand-int 256))]
     (byte-array rand-ints)))
+
+(defn get-random-padding []
+  (let [n (+ 5 (rand-int 5))]
+    (take n (repeatedly #(rand-int 256)))))
 
 (defn aes-encrypt-random [plain]
   (let [k (gen-aes-key)
-        plain' (if (= 0 (mod (count plain) 16))
-                 plain
-                 (st/join (map char (pkcs7-pad plain 16))))]
-    (aes-ecb :encrypt k plain')))
+        plain' (map int plain)
+        rand-pad-pre (get-random-padding)
+        rand-pad-post (get-random-padding)
+        rand-padded (flatten (list rand-pad-pre plain' rand-pad-post))
+        partitioned (partition-all 16 rand-padded)
+        padded (map #(pkcs7-pad % 16) partitioned)
+        b (byte-array (flatten padded))]
+    (aes-ecb :encrypt k b)))
